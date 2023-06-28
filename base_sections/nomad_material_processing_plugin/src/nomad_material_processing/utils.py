d5eda11219cd2ae083a7310e4bac997c498ce0b2
Hampus Näsström <hampus.nasstrom@gmail.com>
2023-06-28 08:28:31 +0200

    Changed `create_archive` Utils Function
    Changed to only check if file exists when running on server

--- base_sections/nomad_material_processing_plugin/src/nomad_material_processing/utils.py
+++ base_sections/nomad_material_processing_plugin/src/nomad_material_processing/utils.py
@@ -17,27 +17,26 @@
 #
 
 
-def _get_reference(upload_id, entry_id):
+def get_reference(upload_id, entry_id):
     return f'../uploads/{upload_id}/archive/{entry_id}#data'
 
 
-def _get_entry_id_from_file_name(file_name, archive):
+def get_entry_id_from_file_name(file_name, archive):
     from nomad.utils import hash
     return hash(archive.metadata.upload_id, file_name)
 
 
-def _create_archive(entity, archive, file_name):
+def create_archive(entity, archive, file_name) -> str:
     import json
+    from nomad.datamodel.context import ClientContext
+    if isinstance(archive.m_context, ClientContext):
+        return None
     if not archive.m_context.raw_path_exists(file_name):
         entity_entry = entity.m_to_dict(with_root_def=True)
         with archive.m_context.raw_file(file_name, 'w') as outfile:
             json.dump({"data": entity_entry}, outfile)
         archive.m_context.process_updated_raw_file(file_name)
-
-
-def create_archive(entity, archive, file_name) -> str:
-    _create_archive(entity, archive, file_name)
-    return _get_reference(
+    return get_reference(
         archive.metadata.upload_id,
-        _get_entry_id_from_file_name(file_name, archive)
+        get_entry_id_from_file_name(file_name, archive)
     )
