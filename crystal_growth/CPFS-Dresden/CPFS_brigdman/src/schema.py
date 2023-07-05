5fc67346f7367f3b80f3403b978af392405ede05
JonathanNoky <Jonathan.Noky@cpfs.mpg.de>
2023-07-05 14:04:14 +0200

    Added minimal CPFSBridgmanTechnique Plugin Added skeleton files __init__.py, nomad_plugin.yaml and schema.py to crystal_growth/CPFS-Dresden/CPFS-Bridgman

---
+++ crystal_growth/CPFS-Dresden/CPFS_brigdman/src/schema.py
@@ -0,0 +1,68 @@
+#
+# Copyright The NOMAD Authors.
+#
+# This file is part of NOMAD. See https://nomad-lab.eu for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+import re
+import datetime
+
+from typing import Union
+from nomad_material_processing import (
+    Substrate,
+    ThinFilmStack,
+)
+from nomad_material_processing.crystal_growth import (
+    BridgmanTechnique,
+)
+from nomad_material_processing.utils import (
+    create_archive,
+)
+from structlog.stdlib import (
+    BoundLogger,
+)
+from nomad.units import (
+    ureg,
+)
+from nomad.metainfo import (
+    Package,
+    Quantity,
+    Section,
+    SubSection,
+)
+from nomad.datamodel.data import (
+    ArchiveSection,
+    EntryData,
+)
+from nomad.datamodel.metainfo.annotations import (
+    ELNAnnotation,
+    BrowserAnnotation,
+    SectionProperties,
+)
+from nomad.datamodel.metainfo.eln import (
+    SampleID,
+    Substance,
+    Component,
+    Ensemble,
+)
+
+m_package = Package(name='MPI CPFS BRIDGMAN')
+
+
+class CPFSBridgmanTechnique(BridgmanTechnique):
+    pass
+
+m_package.__init_metainfo__()
