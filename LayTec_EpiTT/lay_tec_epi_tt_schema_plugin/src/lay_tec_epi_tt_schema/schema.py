da63028e31dbd0f2e27af623b08b4261c41687d9
Sebastian Brueckner <sebasdaszebra@gmail.com>
2023-08-24 21:44:31 +0200

    updated epiTT schema according to base sections

--- LayTec_EpiTT/lay_tec_epi_tt_schema_plugin/src/lay_tec_epi_tt_schema/schema.py
+++ LayTec_EpiTT/lay_tec_epi_tt_schema_plugin/src/lay_tec_epi_tt_schema/schema.py
@@ -35,58 +35,49 @@ from nomad.datamodel.data import (
     EntryData,
     ArchiveSection,
 )
+from nomad.datamodel.data import (
+    EntryDataCategory,
+)
+from nomad.metainfo.metainfo import (Category)
+
 
 m_package = Package(name='LayTec EpiTT Schema')
-'''
- 'CurveDistanceY'],
- 'REFLEC_WAVELENGTH': '633.14',
- 'REFLEC_BANDWIDTH': '3.33',
- 'PYRO_WAVELENGTH': '951.29',
- 'PYRO_BANDWIDTH': '10.01',
- 'WHITE_WAVELENGTH': '405.14',
- 'WHITE_BANDWIDTH': '2.15',
- 'COLS': '58',
- 'TIME': '2020-08-27-11-11-30',
- 'MODULE_NAME': 'Ring TT1 1',
- 'WAFER_LABEL': 'Al Zone 1 (Center)',
- 'WAFER_ZONE': 'Center',
- 'WAFER': '1',
- 'RUN_ID': '200827_158_TEOS',
- 'RUNTYPE_ID': '20',
- 'RUNTYPE_NAME': 'AlGa510mm90',
 
- >>>> 1. BEGIN
->>>> 2. PyroTemp
->>>> 3. DetWhite
->>>> 4. DetReflec
->>>> 5. RLo
-'''
+class IKZLayTecEpiTTCategory(EntryDataCategory):
+    m_def = Category(label='IKZ LayTec EpiTT', categories=[EntryDataCategory])
 
-class LayTecEpiTT_process_time(ArchiveSection):
-    '''
-    LayTec's EpiTT is an emissivity-corrected pyrometer and
-    reflectance measurement for in-situ measurement during
-    growth processes (https://www.laytec.de/epitt)
-    '''
-    process_time = Quantity(
-        type=np.dtype(np.float64),
-        unit = "seconds",
-        shape = ['*'],
-        )
+
+
+# class LayTecEpiTT_process_time(ArchiveSection):
+#     '''
+#     LayTec's EpiTT is an emissivity-corrected pyrometer and
+#     reflectance measurement for in-situ measurement during
+#     growth processes (https://www.laytec.de/epitt)
+#     '''
+#     process_time = Quantity(
+#         type=np.dtype(np.float64),
+#         unit = "seconds",
+#         shape = ['*'],
+#         )
 
 class ReflectanceWavelengthTransient(ArchiveSection):
     m_def = Section(
         a_eln=dict(lane_width='600px'),
         label_quantity="wavelength",
-        a_plot=[
-            {'label': 'EpiTT transient',
-             'x': 'process_time',
-             'y': 'intensity'
-            }]
+        #a_plot=[
+        #    {'label': 'EpiTT transient',
+        #     'x': 'process_time',
+        #     'y': 'intensity'
+        #    }]
         )
     wavelength = Quantity(
-        type=str,
-        description='Reflectanec Wavelength',
+        type=np.dtype(np.int64),#str,
+        unit="nanometer",
+        description='Reflectance Wavelength',
+        #a_eln=ELNAnnotation(
+        #    component='NumberEditQuantity',
+        #    defaultDisplayUnit='nanometer',
+        #),
         )
     wavelength_name = Quantity(
         type=str,
@@ -97,29 +88,37 @@ class ReflectanceWavelengthTransient(ArchiveSection):
     #    unit = "",
     #    shape = ['n_values'],
     #    )
-    process_time = Quantity(
-        type=LayTecEpiTT_process_time.process_time, #dict(type_kind='reference',type_data='#/definitions/section_definitions/1/quantities/8'),
-        default = '#data/process_time'
-        )
+    # process_time = Quantity(
+    #     type=LayTecEpiTT_process_time.process_time, #dict(type_kind='reference',type_data='#/definitions/section_definitions/1/quantities/8'),
+    #     default = '#data/process_time'
+    #     )
     intensity= Quantity(
         type=np.dtype(np.float64),
         #unit = "µm",
         shape = ['*'],
-        a_plot={
-            'x': 'process_time', 'y': 'intensity'
-        })
+        #a_plot={
+        #    'x': 'process_time', 'y': 'intensity'
+    #    }
+        )
+class MeasurementResults(ArchiveSection):
+    process_time = Quantity(
+        type=np.dtype(np.float64),
+        unit = "seconds",
+        shape = ['*'],
+        )
+    # process_time = Quantity(
+    #     type=LayTecEpiTT_process_time.process_time, #dict(type_kind='reference',type_data='#/definitions/section_definitions/1/quantities/8'),
+    #     default = '#data/process_time'
+    #     )
+    pyrometer_temperature = Quantity(
+        type=np.dtype(np.float64),
+        description="PyroTemp transient. LayTec's TrueTemperature of the substrate surface --> Emissivity-corrected pyrometer ",
+        unit = "°C",
+        shape = ['*'],
+        )
+    reflectance_wavelengths = SubSection(section_def=ReflectanceWavelengthTransient, repeats=True)
 
-class LayTecEpiTT(Measurement, LayTecEpiTT_process_time):
-    '''
-    LayTec's EpiTT is an emissivity-corrected pyrometer and
-    reflectance measurement for in-situ measurement during
-    growth processes (https://www.laytec.de/epitt)
-    '''
-    method = Quantity(
-        type=str,
-        description='Method used to collect the data',
-        default='LayTec_EpiTT')
-    #'TIME': '2020-08-27-11-11-30',
+class MeasurementSettings(ArchiveSection):
     module_name = Quantity(
         type=str, #'Ring TT1 1',
         description='MODULE_NAME',
@@ -136,10 +135,6 @@ class LayTecEpiTT(Measurement, LayTecEpiTT_process_time):
         type=str, #'1',
         description='WAFER',
         )
-    run_ID = Quantity(
-        type=str, #'200827_158_TEOS',
-        description='RUNTYPE_ID',
-        )
     runtype_ID = Quantity(
         type=str, #'20',
         description='RUNTYPE_ID',
@@ -148,18 +143,67 @@ class LayTecEpiTT(Measurement, LayTecEpiTT_process_time):
         type=str, #'AlGa510mm90',
         description='RUNTYPE_NAME',
         )
+
+class LayTecEpiTT(Measurement ):
+    '''
+    LayTec's EpiTT is an emissivity-corrected pyrometer and
+    reflectance measurement for in-situ measurement during
+    growth processes (https://www.laytec.de/epitt)
+    '''
+    method = Quantity(
+        type=str,
+        description='Method used to collect the data',
+        default='LayTec_EpiTT')
+    #'TIME': '2020-08-27-11-11-30',
+    # module_name = Quantity(
+    #     type=str, #'Ring TT1 1',
+    #     description='MODULE_NAME',
+    #     )
+    # wafer_label = Quantity(
+    #     type=str, #'Al Zone 1 (Center)',
+    #     description='WAFER_LABEL',
+    #     )
+    # wafer_zone = Quantity(
+    #     type=str, #'Center',
+    #     description='WAFER_ZONE',
+    #     )
+    # wafer = Quantity(
+    #     type=str, #'1',
+    #     description='WAFER',
+    #     )
+    run_ID = Quantity(
+        type=str, #'200827_158_TEOS',
+        description='RUNTYPE_ID',
+        )
+    location = Quantity(
+        type=str,
+        description='''
+        The location of the process in longitude, latitude.
+        ''',
+        default='52.431685, 13.526855', #IKZ coordinates
+    )
+    # runtype_ID = Quantity(
+    #     type=str, #'20',
+    #     description='RUNTYPE_ID',
+    #     )
+    # runtype_name = Quantity(
+    #     type=str, #'AlGa510mm90',
+    #     description='RUNTYPE_NAME',
+    #     )
     # process_time = Quantity(
     #     type=np.dtype(np.float64),
     #     unit = "seconds",
     #     shape = ['n_values'],
     #     )
-    pyrometer_temperature = Quantity(
-        type=np.dtype(np.float64),
-        description="PyroTemp transient. LayTec's TrueTemperature of the substrate surface --> Emissivity-corrected pyrometer ",
-        unit = "°C",
-        shape = ['*'],
-        )
-    reflectance_wavelength = SubSection(section_def=ReflectanceWavelengthTransient, repeats=True)
+    # pyrometer_temperature = Quantity(
+    #     type=np.dtype(np.float64),
+    #     description="PyroTemp transient. LayTec's TrueTemperature of the substrate surface --> Emissivity-corrected pyrometer ",
+    #     unit = "°C",
+    #     shape = ['*'],
+    #     )
+    measurement_settings = SubSection(section_def=MeasurementSettings)
+    results =  SubSection(section_def=MeasurementResults, label="Results")
+    #reflectance_wavelength = SubSection(section_def=ReflectanceWavelengthTransient, repeats=True)
 
     #growth_analysis = SubSection(section)
 
@@ -170,13 +214,14 @@ class LayTec_EpiTT_Measurement(LayTecEpiTT, EntryData, ArchiveSection):
     m_def = Section(
         a_eln=dict(lane_width='600px'),
         a_plot=[
-            {'label': 'EpiTT transients',
-             'x': 'process_time',
-             'y': ['pyrometer_temperature','reflectance_wavelength/:/intensity'],
+            {#'label': 'EpiTT transients',
+             'x': 'results/:/process_time',
+             'y': ['results/:/pyrometer_temperature','results/:/reflectance_wavelengths/:/intensity'],
              #'layout': {'yaxis': {'type': 'log'}},
-            },]
-        )
-
+            },],
+        categories=[IKZLayTecEpiTTCategory],
+        label='EpiTT Measurement',
+    )
     data_file = Quantity(
         type=str,
         description='Data file containing the EpiTT data (*.dat)',
@@ -222,31 +267,34 @@ class LayTec_EpiTT_Measurement(LayTecEpiTT, EntryData, ArchiveSection):
                     self.lab_id = name_string[1:]
                 if "TIME" in epitt_data[0].keys():
                     self.datetime = datetime.strptime(epitt_data[0]["TIME"],'%Y-%m-%d-%H-%M-%S') #'2020-08-27-11-11-30',
+                self.measurement_settings = MeasurementSettings() #?
                 if "MODULE_NAME" in epitt_data[0].keys():
-                    self.module_name = epitt_data[0]["MODULE_NAME"]
+                    self.measurement_settings.module_name = epitt_data[0]["MODULE_NAME"]
                 if "WAFER_LABEL" in epitt_data[0].keys():
-                    self.wafer_label = epitt_data[0]["WAFER_LABEL"]
+                    self.measurement_settings.wafer_label = epitt_data[0]["WAFER_LABEL"]
                 if "WAFER_ZONE" in epitt_data[0].keys():
-                    self.wafer_zone = epitt_data[0]["WAFER_ZONE"]
+                    self.measurement_settings.wafer_zone = epitt_data[0]["WAFER_ZONE"]
                 if "WAFER" in epitt_data[0].keys():
-                    self.wafer = epitt_data[0]["WAFER"]
+                    self.measurement_settings.wafer = epitt_data[0]["WAFER"]
                 if "RUN_ID" in epitt_data[0].keys():
                     self.run_ID = epitt_data[0]["RUN_ID"]
                 if "RUNTYPE_ID" in epitt_data[0].keys():
-                    self.runtype_ID = epitt_data[0]["RUNTYPE_ID"]
+                    self.measurement_settings.runtype_ID = epitt_data[0]["RUNTYPE_ID"]
                 if "RUNTYPE_NAME" in epitt_data[0].keys():
-                    self.runtype_name = epitt_data[0]["RUNTYPE_NAME"]
+                    self.measurement_settings.runtype_name = epitt_data[0]["RUNTYPE_NAME"]
                 #self.time_transient = epitt_data[1]["BEGIN"]
-                self.process_time = epitt_data[1]["BEGIN"]
-                self.pyrometer_temperature = epitt_data[1]["PyroTemp"]
-                self.reflectance_wavelength = []
+                results = MeasurementResults()
+                results.process_time = epitt_data[1]["BEGIN"]
+                results.pyrometer_temperature = epitt_data[1]["PyroTemp"]
+                results.reflectance_wavelengths = []
                 for wl, datacolname in zip(['REFLEC_WAVELENGTH', 'PYRO_WAVELENGTH', 'WHITE_WAVELENGTH'],["DetReflec", "RLo", "DetWhite"]):
                     if wl in epitt_data[0].keys():
                         transient_object = ReflectanceWavelengthTransient()
-                        transient_object.wavelength = epitt_data[0][wl]
+                        transient_object.wavelength = int(round(float(epitt_data[0][wl])))
                         transient_object.wavelength_name = wl#epitt_data[0]["REFLEC_WAVELENGTH"]
                         transient_object.intensity = epitt_data[1][datacolname]
-                        self.reflectance_wavelength.append(transient_object)
+                        results.reflectance_wavelengths.append(transient_object)
+                self.results = [results]
 
         #def rolling_average_insitu_reflectance(wavelength, start, period):
         #    df_refl_data=pd.DataFrame([self.process_time,self.)
