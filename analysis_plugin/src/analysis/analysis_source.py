af3993e1b5b315e03b8255f3dbae519f144d4c20
Sarthak Kapoor <sarthakkapoor@hotmail.com>
2023-12-29 16:52:54 +0100

    Added xrd analysis functions

--- analysis_plugin/src/analysis/analysis_source.py
+++ analysis_plugin/src/analysis/analysis_source.py
@@ -37,7 +37,10 @@ def get_entry_archive_data(token_header: dict, base_url: str, entry_id: str) -> 
     }
     response = requests.post(
         f"{base_url}/entries/{entry_id}/archive/query",
-        headers = token_header,
+        headers = {
+            **token_header,
+            'Accept': 'application/json'
+        },
         json = query
     ).json()
     if 'data' in response.keys():
@@ -45,17 +48,19 @@ def get_entry_archive_data(token_header: dict, base_url: str, entry_id: str) -> 
     return response
 
 @category('XRD')
-def xrd_plot_intensity_two_theta(archive_data: dict) -> None:
+def xrd_plot_intensity_two_theta(archive_data: dict, peak_indices = None) -> None:
     '''
     Generates a 2D plot of intensity vs 2θ with linear x and y axis.
 
     Args:
         archive_data (dict): Archive data of the entry.
+        peak_indices (np.array): Indices of peaks found in the intensity data.
     '''
     import plotly.express as px
+    import numpy as np
 
-    intensity = archive_data['results'][0]['intensity']
-    two_theta = archive_data['results'][0]['two_theta']
+    intensity = np.array(archive_data['results'][0]['intensity'])
+    two_theta = np.array(archive_data['results'][0]['two_theta'])
 
     line_linear = px.line(
             x=two_theta,
@@ -66,20 +71,34 @@ def xrd_plot_intensity_two_theta(archive_data: dict) -> None:
             },
             title='Intensity vs 2θ (linear scale)',
         )
+    if peak_indices is not None or len(peak_indices) > 0:
+        line_linear.add_scatter(
+            x=two_theta[peak_indices],
+            y=intensity[peak_indices],
+            mode='markers',
+            marker=dict(
+                size=8,
+                color='red',
+                symbol='cross'
+            ),
+            name='Peaks',
+        )
     line_linear.show()
 
 @category('XRD')
-def xrd_plot_logy_intensity_two_theta(archive_data: dict) -> None:
+def xrd_plot_logy_intensity_two_theta(archive_data: dict, peak_indices = None) -> None:
     '''
     Generates a 2D plot of intensity vs 2θ with linear x and log y axis.
 
     Args:
         archive_data (dict): Archive data of the entry.
+        peak_indices (np.array): Indices of peaks found in the intensity data.
     '''
     import plotly.express as px
+    import numpy as np
 
-    intensity = archive_data['results'][0]['intensity']
-    two_theta = archive_data['results'][0]['two_theta']
+    intensity = np.array(archive_data['results'][0]['intensity'])
+    two_theta = np.array(archive_data['results'][0]['two_theta'])
 
     line_log = px.line(
         x=two_theta,
@@ -91,4 +110,94 @@ def xrd_plot_logy_intensity_two_theta(archive_data: dict) -> None:
         },
         title='Intensity vs 2θ (log scale)',
     )
+    if peak_indices is not None or len(peak_indices) > 0:
+        line_log.add_scatter(
+            x=two_theta[peak_indices],
+            y=intensity[peak_indices],
+            mode='markers',
+            marker=dict(
+                size=8,
+                color='red',
+                symbol='cross'
+            ),
+            name='Peaks',
+        )
     line_log.show()
+
+@category('XRD')
+def xrd_find_peaks(archive_data: dict, options: dict = None) -> dict:
+    '''
+    Finds the peaks in the intensity vs 2θ plot.
+
+    Args:
+        archive_data (dict): Archive data of the entry.
+        options (dict): Options for the peak finding algorithm `scipy.signal.find_peaks`.
+
+    Returns:
+        dict: Peaks found in the intensity vs 2θ plot.
+    '''
+    import numpy as np
+    from scipy.signal import find_peaks
+
+    intensity = np.array(archive_data['results'][0]['intensity'])
+    two_theta = np.array(archive_data['results'][0]['two_theta'])
+
+    if options:
+        peak_indices, _ = find_peaks(intensity, **options)
+    else:
+        peak_indices, _ = find_peaks(intensity)
+
+    peaks_intensity = intensity[peak_indices]
+    peaks_two_theta = two_theta[peak_indices]
+
+    peaks = {
+        'peaks': {
+            'intensity': peaks_intensity.tolist(),
+            'two_theta': peaks_two_theta.tolist(),
+        }
+    }
+
+    return peaks, peak_indices
+
+@category('XRD')
+def xrd_save_analysis_results(
+    results: dict, file_name: str = 'tmp_analysis_results.json'
+):
+    '''
+    Saves the analysis results as a json file.
+
+    Args:
+        results (dict): Analysis results.
+        file_name (str): Name of the file to save the results.
+    '''
+    import json
+
+    with open(file_name, 'w', encoding='utf-8') as f:
+        json.dump(results, f)
+
+@category('XRD')
+def xrd_conduct_analysis(archive_data: dict, plot: bool = True) -> None:
+    '''
+    Conducts XRD analysis on the given archive data. Also saves the analysis results as
+    a json file which can be used to fill `analysis_results` section.
+
+    Args:
+        archive_data (dict): Archive data of the entry.
+        plot (bool): If True, plots the intensity vs 2θ plot.
+    '''
+    import collections
+
+    options = {
+        'height': 20,
+        'threshold': 30,
+        'distance': 1,
+    }
+    peaks, peak_indices = xrd_find_peaks(archive_data, options = options)
+    if plot:
+        xrd_plot_intensity_two_theta(archive_data, peak_indices)
+        xrd_plot_logy_intensity_two_theta(archive_data, peak_indices)
+
+    results = collections.defaultdict(None)
+    results['peaks'] = peaks
+
+    xrd_save_analysis_results(results)
