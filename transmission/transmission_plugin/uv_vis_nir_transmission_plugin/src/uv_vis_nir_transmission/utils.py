26e121b387ec3d3fd5ca2ff0899cad48d052c826
Sarthak Kapoor <sarthakkapoor@hotmail.com>
2024-03-25 15:09:23 +0100

    Adding utils from nomad_measurement.xrd

--- transmission/transmission_plugin/uv_vis_nir_transmission_plugin/src/uv_vis_nir_transmission/utils.py
+++ transmission/transmission_plugin/uv_vis_nir_transmission_plugin/src/uv_vis_nir_transmission/utils.py
@@ -62,3 +62,29 @@ def merge_sections(
                 logger.warning(warning)
             else:
                 print(warning)
+
+
+def get_reference(upload_id, entry_id):
+    return f'../uploads/{upload_id}/archive/{entry_id}#data'
+
+
+def get_entry_id_from_file_name(file_name, archive):
+    from nomad.utils import hash
+
+    return hash(archive.metadata.upload_id, file_name)
+
+
+def create_archive(entity, archive, file_name) -> str:
+    import json
+    from nomad.datamodel.context import ClientContext
+
+    if isinstance(archive.m_context, ClientContext):
+        return None
+    if not archive.m_context.raw_path_exists(file_name):
+        entity_entry = entity.m_to_dict(with_root_def=True)
+        with archive.m_context.raw_file(file_name, 'w') as outfile:
+            json.dump({'data': entity_entry}, outfile)
+        archive.m_context.process_updated_raw_file(file_name)
+    return get_reference(
+        archive.metadata.upload_id, get_entry_id_from_file_name(file_name, archive)
+    )
