b7a092c79fda396f1ff2b117654c3dcdeaa6253c
Sarthak Kapoor <57119427+ka-sarthak@users.noreply.github.com>
2024-03-28 09:50:27 +0100

    127 add transmission plugin (#151)
    * Add UV-Vis-NIR transmission plugin files
    
    * Add test.py and readers.py files for UV-Vis-NIR transmission plugin
    
    * Update dependencies and add optional dependencies
    
    * Copy reader code from pynxtools
    
    * Add utils, ruff, modify imports
    
    * Modify reader: extract more data, remove calculation of ranges
    
    * Refactoring, renaming based on Hiroki's input
    
    * Add units for numeric quantities; dimensionless for percentage
    
    * Add read_detector_module
    
    * ruff format schema.py
    
    * Refactor imports, refactor sections, add default units
    
    * Modify write function, schema
    
    * Minor changes
    
    * Move plotting to method in UVVisTransmissionResult
    
    * Rearrange sections
    
    * Add ChangePoint class, modify change_point quantities
    
    * Minor fixes
    
    * Modify Instrument class
    
    * Minor fixes
    
    * Adding utils from nomad_measurement.xrd
    
    * Adding methods to handle the instrument reference
    
    * change points are list of wavelengths
    
    * Modify plot
    
    ---------
    
    Co-authored-by: Sebastian Brueckner <sebasdaszebra@gmail.com>

---
+++ transmission/transmission_plugin/uv_vis_nir_transmission_plugin/src/uv_vis_nir_transmission/readers.py
@@ -0,0 +1,264 @@
+#
+# Copyright The NOMAD Authors.
+#
+# This file is part of NOMAD. See https://nomad-lab.eu for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from typing import Dict, Any, TYPE_CHECKING
+from collections import defaultdict
+from datetime import datetime
+from inspect import isfunction
+import numpy as np
+from nomad.units import ureg
+import pandas as pd
+
+if TYPE_CHECKING:
+    from structlog.stdlib import (
+        BoundLogger,
+    )
+
+
+def read_sample_name(metadata: list) -> str:
+    """Reads the sample name from the metadata"""
+    if not metadata[2]:
+        return None
+    return metadata[2].split('.')[0]
+
+
+def read_start_datetime(metadata: list) -> str:
+    """Reads the start date from the metadata"""
+    if not metadata[3] or not metadata[4]:
+        return None
+    century = str(datetime.now().year // 100)
+    formated_date = metadata[3].replace('/', '-')
+    return f'{century}{formated_date}T{metadata[4]}000Z'
+
+
+def read_is_D2_lamp_used(metadata: list) -> bool:
+    """Reads whether the D2 lamp was active during the measurement"""
+    if not metadata[21]:
+        return None
+    return bool(float(metadata[21]))
+
+
+def read_is_tungsten_lamp_used(metadata: list) -> bool:
+    """Reads whether the tungsten lamp was active during the measurement"""
+    if not metadata[22]:
+        return None
+    return bool(float(metadata[22]))
+
+
+def read_sample_attenuation_percentage(metadata: list) -> int:
+    """Reads the sample attenuation percentage from the metadata"""
+    if not metadata[47]:
+        return None
+    return int(metadata[47].split()[0].split(':')[1]) * ureg.dimensionless
+
+
+def read_reference_attenuation_percentage(metadata: list) -> int:
+    """Reads the sample attenuation percentage from the metadata"""
+    if not metadata[47]:
+        return None
+    return int(metadata[47].split()[1].split(':')[1]) * ureg.dimensionless
+
+
+def read_is_depolarizer_on(metadata: list) -> bool:
+    """Reads whether the depolarizer was active during the measurement"""
+    if not metadata[46]:
+        return False
+    return metadata[46] == 'on'
+
+
+def read_long_line(line: str) -> Dict[str, float]:
+    """A long line in .asc file is defined as one containing values of a quantity at
+    multiple wavelengths. These values are available within one line but separated by
+    whitespaces. This function generates a dict where each key-value
+    pair is the value for the corresponding wavelength (key).
+    Eg. {"600": 0.5, "1050": 1.0}
+
+    Args:
+        line (str): The line to parse.
+
+    Returns:
+        Dict[str, float]: The dict of detector settings with wavelength as key.
+    """
+
+    def convert(val):
+        val_list = val.strip().split('/')
+        return {val_list[0]: float(val_list[1])}
+
+    output = dict()
+    for s in line.split():
+        output.update(convert(s))
+    return output
+
+
+def read_monochromator_slit_width(metadata: list) -> Dict[str, float]:
+    """Reads the monochromator slit width from the metadata"""
+    if not metadata[31]:
+        return None
+    output_dict = read_long_line(metadata[31])
+    for key in output_dict:
+        output_dict[key] *= ureg.nanometer
+    return output_dict
+
+
+def read_detector_integration_time(metadata: list) -> Dict[str, float]:
+    """Reads the detector integration time from the metadata"""
+    if not metadata[32]:
+        return None
+    output_dict = read_long_line(metadata[32])
+    for key in output_dict:
+        output_dict[key] *= ureg.s
+    return output_dict
+
+
+def read_detector_nir_gain(metadata: list) -> Dict[str, float]:
+    """Reads the detector NIR gain from the metadata"""
+    if not metadata[35]:
+        return None
+    output_dict = read_long_line(metadata[35])
+    for key in output_dict:
+        output_dict[key] *= ureg.dimensionless
+    return output_dict
+
+
+def read_detector_change_wavelength(metadata: list) -> list[float]:
+    """Reads the detector change wavelength from the metadata"""
+    if not metadata[43]:
+        return None
+    return np.array([float(x) for x in metadata[43].split()]) * ureg.nanometer
+
+
+def read_polarizer_angle(metadata: list) -> list[float]:
+    """Reads the polarizer angle from the metadata"""
+    if not metadata[48]:
+        return None
+    return float(metadata[48]) * ureg.degree
+
+
+def read_monochromator_change_wavelength(metadata: list) -> list[float]:
+    """Reads the monochromator change wavelength from the metadata"""
+    if not metadata[41]:
+        return None
+    return np.array([float(x) for x in metadata[41].split()]) * ureg.nanometer
+
+
+def read_lamp_change_wavelength(metadata: list) -> list[float]:
+    """Reads the lamp change wavelength from the metadata"""
+    if not metadata[42]:
+        return None
+    return np.array([float(x) for x in metadata[42].split()]) * ureg.nanometer
+
+
+def read_detector_module(metadata: list) -> str:
+    """Reads the detector module from the metadata"""
+    if not metadata[24]:
+        return None
+    if 'uv/vis/nir detector' in metadata[24].lower():
+        return 'three detector module'
+    if '150mm sphere' in metadata[24].lower():
+        return '150-mm integrating sphere'
+    return None
+
+
+METADATA_MAP: Dict[str, Any] = {
+    'sample_name': read_sample_name,
+    'start_datetime': read_start_datetime,
+    'analyst_name': 7,
+    'instrument_name': 11,
+    'instrument_serial_number': 12,
+    'instrument_firmware_version': 13,
+    'is_D2_lamp_used': read_is_D2_lamp_used,
+    'is_tungsten_lamp_used': read_is_tungsten_lamp_used,
+    'sample_beam_position': 44,
+    'common_beam_mask_percentage': 45,
+    'is_common_beam_depolarizer_on': read_is_depolarizer_on,
+    'sample_attenuation_percentage': read_sample_attenuation_percentage,
+    'reference_attenuation_percentage': read_reference_attenuation_percentage,
+    'detector_integration_time': read_detector_integration_time,
+    'detector_NIR_gain': read_detector_nir_gain,
+    'detector_change_wavelength': read_detector_change_wavelength,
+    'detector_module': read_detector_module,
+    'polarizer_angle': read_polarizer_angle,
+    'ordinate_type': 80,
+    'wavelength_units': 79,
+    'monochromator_slit_width': read_monochromator_slit_width,
+    'monochromator_change_wavelength': read_monochromator_change_wavelength,
+    'lamp_change_wavelength': read_lamp_change_wavelength,
+}
+
+
+def restructure_measured_data(data: pd.DataFrame) -> Dict[str, np.ndarray]:
+    """Builds the data entry dict from the data in a pandas dataframe.
+
+    Args:
+        data (pd.DataFrame): The dataframe containing the data.
+
+    Returns:
+        Dict[str, np.ndarray]: The dict with the measured data.
+    """
+    output: Dict[str, Any] = {}
+    output['measured_wavelength'] = data.index.values
+    output['measured_ordinate'] = data.values[:, 0] * ureg.dimensionless
+
+    return output
+
+
+def read_asc(file_path: str, logger: 'BoundLogger' = None) -> Dict[str, Any]:
+    """
+    Function for reading the transmission data from PerkinElmer *.asc.
+
+    Args:
+        file_path (str): The path to the transmission data file.
+        logger (BoundLogger, optional): A structlog logger. Defaults to None.
+
+    Returns:
+        Dict[str, Any]: The transmission data and metadata in a Python dictionary.
+    """
+
+    output: Dict[str, Any] = defaultdict(lambda: None)
+    data_start_ind = '#DATA'
+
+    with open(file_path, encoding='utf-8') as file_obj:
+        metadata = []
+        for line in file_obj:
+            if line.strip() == data_start_ind:
+                break
+            metadata.append(line.strip())
+
+        data = pd.read_csv(file_obj, delim_whitespace=True, header=None, index_col=0)
+
+    for path, val in METADATA_MAP.items():
+        # If the dict value is an int just get the data with it's index
+        if isinstance(val, int):
+            if metadata[val]:
+                try:
+                    output[path] = float(metadata[val]) * ureg.dimensionless
+                except ValueError:
+                    output[path] = metadata[val]
+        elif isinstance(val, str):
+            output[path] = val
+        elif isfunction(val):
+            output[path] = val(metadata)
+        else:
+            raise ValueError(
+                f"Invalid type value {type(val)} of entry '{path}:{val}' in METADATA_MAP"
+            )
+
+    output.update(restructure_measured_data(data))
+    output['measured_wavelength'] *= ureg(output['wavelength_units'])
+
+    return output
---
+++ transmission/transmission_plugin/uv_vis_nir_transmission_plugin/src/uv_vis_nir_transmission/utils.py
@@ -0,0 +1,90 @@
+from typing import (
+    TYPE_CHECKING,
+)
+
+if TYPE_CHECKING:
+    from nomad.datamodel.data import (
+        ArchiveSection,
+    )
+    from structlog.stdlib import (
+        BoundLogger,
+    )
+
+
+def merge_sections(
+    section: 'ArchiveSection',
+    update: 'ArchiveSection',
+    logger: 'BoundLogger' = None,
+) -> None:
+    if update is None:
+        return
+    if section is None:
+        section = update.m_copy()
+        return
+    if not isinstance(section, type(update)):
+        raise TypeError(
+            'Cannot merge sections of different types: '
+            f'{type(section)} and {type(update)}'
+        )
+    for name, quantity in update.m_def.all_quantities.items():
+        if not update.m_is_set(quantity):
+            continue
+        if not section.m_is_set(quantity):
+            section.m_set(quantity, update.m_get(quantity))
+        elif (
+            quantity.is_scalar
+            and section.m_get(quantity) != update.m_get(quantity)
+            or quantity.repeats
+            and (section.m_get(quantity) != update.m_get(quantity)).any()
+        ):
+            warning = f'Merging sections with different values for quantity "{name}".'
+            if logger:
+                logger.warning(warning)
+            else:
+                print(warning)
+    for name, sub_section_def in update.m_def.all_sub_sections.items():
+        count = section.m_sub_section_count(sub_section_def)
+        if count == 0:
+            for update_sub_section in update.m_get_sub_sections(sub_section_def):
+                section.m_add_sub_section(sub_section_def, update_sub_section)
+        elif count == update.m_sub_section_count(sub_section_def):
+            for i in range(count):
+                merge_sections(
+                    section.m_get_sub_section(sub_section_def, i),
+                    update.m_get_sub_section(sub_section_def, i),
+                    logger,
+                )
+        elif update.m_sub_section_count(sub_section_def) > 0:
+            warning = (
+                f'Merging sections with different number of "{name}" sub sections.'
+            )
+            if logger:
+                logger.warning(warning)
+            else:
+                print(warning)
+
+
+def get_reference(upload_id, entry_id):
+    return f'../uploads/{upload_id}/archive/{entry_id}#data'
+
+
+def get_entry_id_from_file_name(file_name, archive):
+    from nomad.utils import hash
+
+    return hash(archive.metadata.upload_id, file_name)
+
+
+def create_archive(entity, archive, file_name) -> str:
+    import json
+    from nomad.datamodel.context import ClientContext
+
+    if isinstance(archive.m_context, ClientContext):
+        return None
+    if not archive.m_context.raw_path_exists(file_name):
+        entity_entry = entity.m_to_dict(with_root_def=True)
+        with archive.m_context.raw_file(file_name, 'w') as outfile:
+            json.dump({'data': entity_entry}, outfile)
+        archive.m_context.process_updated_raw_file(file_name)
+    return get_reference(
+        archive.metadata.upload_id, get_entry_id_from_file_name(file_name, archive)
+    )
