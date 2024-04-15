5e85e3fcda6c4787283d91177335555b3ca5052b
Sarthak Kapoor <sarthakkapoor@hotmail.com>
2024-04-15 16:59:54 +0200

    Transmission: use datetime data in quantity

--- transmission/transmission_plugin/uv_vis_nir_transmission_plugin/src/uv_vis_nir_transmission/schema.py
+++ transmission/transmission_plugin/uv_vis_nir_transmission_plugin/src/uv_vis_nir_transmission/schema.py
@@ -509,6 +509,7 @@ class ELNUVVisTransmission(UVVisTransmission, PlotSection, EntryData):
             name=data_dict['instrument_name'],
             serial_number=data_dict['instrument_serial_number'],
             software_version=data_dict['instrument_firmware_version'],
+            datetime=data_dict['start_datetime'],
         )
         instrument.normalize(archive, logger)
 
@@ -590,6 +591,7 @@ class ELNUVVisTransmission(UVVisTransmission, PlotSection, EntryData):
             logger (BoundLogger): A structlog logger.
         """
         self.user = transmission_dict['analyst_name']
+        self.datetime = transmission_dict['start_datetime']
 
         result = UVVisNirTransmissionResult(
             wavelength=transmission_dict['measured_wavelength'],
