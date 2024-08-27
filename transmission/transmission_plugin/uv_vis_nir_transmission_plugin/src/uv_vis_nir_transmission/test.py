9aeeefb1fb3d563689cdc104a28c42596157649f
Sarthak Kapoor <sarthakkapoor@hotmail.com>
2024-08-27 16:37:56 +0200

    Clean module; move and delete aux files

--- transmission/transmission_plugin/uv_vis_nir_transmission_plugin/src/uv_vis_nir_transmission/test.py
+++
@@ -1,7 +0,0 @@
-from readers import read_nexus_asc
-
-# Replace 'testfile' with the name of your test file
-result = read_nexus_asc('/home/brueckner/schemas/AreaA-data_modeling_and_schemas/transmission/transmission_plugin/uv_vis_nir_transmission_plugin/tests/data/3DM_test01.Probe.Raw.asc')
-
-# Print the result to see what the function returns
-print(result)
--- transmission/transmission_plugin/uv_vis_nir_transmission_plugin/tests/data/README.md
+++
@@ -1,3 +0,0 @@
-This is a directory where you can store any data needed for running the tests.
-
-Feel free to delete this README file.
